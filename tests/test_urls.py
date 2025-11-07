a17d6d416020436548164a78d7e4d2dddf3d6d27
pgreenleaf <pgreenleaf@drew.edu>
2025-11-07 12:49:19 -0500

    Most recent push

---
+++ tests/test_urls.py
@@ -0,0 +1,8 @@
+import unittest
+
+class BasicUrlTest(unittest.TestCase):
+    def test_always_passes(self):
+        """
+        A very basic test that always passes.
+        """
+        self.assertTrue(True)
