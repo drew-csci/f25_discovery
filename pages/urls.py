43997ff499d55d41d5e76f12b9415d95bd82fa30
morcos <morcoscharlotte@gmail.com>
2025-12-08 12:48:17 -0500

    chore: Add buggy search route

--- pages/urls.py
+++ pages/urls.py
@@ -1,13 +1,14 @@
+# pages/urls.py
 from django.urls import path
 from . import views
+from . import buggy_view   # <-- add this import
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('screen1/', views.screen1, name='screen1'),
     path('screen2/', views.screen2, name='screen2'),
     path('screen3/', views.screen3, name='screen3'),
-    path('notifications/', views.notifications, name='notifications'), # added for notifications page, 
-    # URL for the Company Home Page (Dashboard)
+    path('notifications/', views.notifications, name='notifications'),
     path('company/home/', views.company_home, name='company_home'),
-    path('university/home/', views.university_home, name='university_home'),
+    path('buggy/', buggy_view.buggy_search, name='buggy_search'),  # <-- add this route
 ]
